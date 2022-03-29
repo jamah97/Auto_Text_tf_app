d5d4e9806c13d90c5f1f16c6347f0f4b382f3eb2
jamah97 <88891435+jamah97@users.noreply.github.com>
2022-03-29 01:43:03 -0500

    Update auto_text_tf_app.py

--- auto_text_tf_app.py
+++ auto_text_tf_app.py
@@ -23,7 +23,7 @@ def main():
 				st.dataframe(df.head(int(st.text_input("Select Number of Rows To View"))))
 			except ValueError:
 				pass
-		algorithm = ["nnlm-en-dim20", "nnlm-en-dim50", "nnlm-en-dim128"]
+		algorithm = ["nnlm-en-dim20", "nnlm-en-dim50", "nnlm-en-dim128", "Wiki-words-500-with-normalization"]
 		choice = st.selectbox("Select Algorithm",algorithm)
 		testsize = st.slider('Testing size: Select percent of data that will be used as testing data.', 0, 50)
 
@@ -138,6 +138,44 @@ def main():
 				st.write('Test loss:', score[0])
 				st.write('Test accuracy:', score[1])
 
+
+
+		if choice == "Wiki-words-500-with-normalization":
+			iv = st.selectbox("Select predictor variable", df.columns.to_list())
+			dv = st.selectbox("Select independent variable", df.columns.to_list())
+			iv1 = df[iv]
+			dv1 = df[dv].values
+			dv2 = pd.DataFrame(data=dv1)
+			iv2 = pd.DataFrame(data=iv1)
+
+			loss_function = ["binary_crossentropy", "categorical_crossentropy"]
+			loss = st.selectbox("Select loss function",loss_function)
+
+			if st.button("Model Performance"):
+				train_x, test_x, train_y, test_y = train_test_split(iv2,dv2, test_size=testsize, random_state=1)
+				endcoder = preprocessing.LabelEncoder()
+				train_y = endcoder.fit_transform(train_y)
+				test_y = endcoder.fit_transform(test_y)
+				num_c = np.max(train_y) + 1
+				st.write("number of classes", num_c)
+				train_y = utils.to_categorical(train_y, num_c)
+				test_y = utils.to_categorical(test_y, num_c)
+
+				hub_layer = hub.KerasLayer("https://tfhub.dev/google/Wiki-words-500-with-normalization/2", input_shape=[], dtype=tf.string)
+				model = tf.keras.models.Sequential([ hub_layer,
+													tf.keras.layers.Dense(128, activation='relu'),
+													tf.keras.layers.Dropout(.2),
+													tf.keras.layers.Dense(num_c, activation='sigmoid')])
+
+
+
+				model.compile(optimizer='adam', loss=loss, metrics=['accuracy'])
+
+				history = model.fit(train_x,train_y, epochs=50, batch_size=32,validation_data=(test_x, test_y),verbose=0)
+
+				score = model.evaluate(test_x, test_y, verbose = 0)
+				st.write('Test loss:', score[0])
+				st.write('Test accuracy:', score[1])
 
 
 
